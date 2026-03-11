e577a32662e02fc354c2ca09a34e763478754744
YasseenR <114093367+YasseenR@users.noreply.github.com>
2026-03-11 10:43:39 -0400

    Initial XCode Project Commit

---
+++ PlateApp/PlateApp/PlateAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlateAppApp.swift
+//  PlateApp
+//
+//  Created by Yasseen Rouni on 3/11/26.
+//
+
+import SwiftUI
+
+@main
+struct PlateAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
