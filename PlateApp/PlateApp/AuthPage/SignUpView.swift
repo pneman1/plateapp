4bf74bf5031a789abcdaa985390416e6266fe628
YasseenR <114093367+YasseenR@users.noreply.github.com>
2026-04-02 14:01:04 -0400

    fix inline error messaging on signup page

--- PlateApp/PlateApp/AuthPage/SignUpView.swift
+++ PlateApp/PlateApp/AuthPage/SignUpView.swift
@@ -9,7 +9,6 @@ import SwiftUI
 struct SignUpView: View {
     @EnvironmentObject var viewModel: AuthViewModel
     @State private var confirmedPassword: String = ""
-    @State private var errorMessage: String? = nil
     @State private var attempts: CGFloat = 0
     
     var body: some View {
@@ -46,17 +45,16 @@ struct SignUpView: View {
                     .cornerRadius(6)
                     .overlay(RoundedRectangle(cornerRadius: 6).stroke(Color(.white), lineWidth: 1))
                 
-                if let error = errorMessage {
+                if let error = viewModel.errorMessage {
                     Text(error)
                         .font(.caption)
                         .foregroundStyle(.red)
                         .transition(.opacity)
                 }
             }
-            .modifier(Shake(animatableData: attempts))
             
             Button(action: {
-                login()
+                viewModel.signUp()
             }) {
                 Text("Continue")
                     .fontWeight(.semibold)
@@ -71,34 +69,8 @@ struct SignUpView: View {
             
         }
     }
-    
-    func login() {
-        if viewModel.password.isEmpty || viewModel.email.isEmpty {
-            withAnimation(.default) {
-                self.errorMessage = "Please fill in all fields."
-                self.attempts += 1
-            }
-        } else if viewModel.password != confirmedPassword {
-            self.errorMessage = "Passwords are not the same."
-            self.attempts += 1
-        } else {
-            viewModel.signUp()
-        }
-    }
 }
 
 #Preview{
     SignUpView()
-}
-
-
-struct Shake: GeometryEffect {
-    var amount: CGFloat = 10
-        var shakesPerUnit = 3
-        var animatableData: CGFloat
-
-        func effectValue(size: CGSize) -> ProjectionTransform {
-            ProjectionTransform(CGAffineTransform(translationX:
-                amount * sin(animatableData * .pi * CGFloat(shakesPerUnit)), y: 0))
-        }
 }
